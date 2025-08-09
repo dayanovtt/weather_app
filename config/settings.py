8e8bc00c80e7d0c74f6d24edfd5b7a0167b0fd22
Timur Dayanov <135445176+dayanovtt@users.noreply.github.com>
2025-08-09 15:37:16 +0300

    fix settings

--- config/settings.py
+++ config/settings.py
@@ -1,10 +1,12 @@
 from pathlib import Path
+from typing import Optional
+
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from pydantic import Field
 
 
 class Settings(BaseSettings):
-    api_key: str = Field(..., alias="API_KEY")
+    api_key: Optional[str] = Field(None, alias="API_KEY")
     geo_url: str = "http://api.openweathermap.org/geo/1.0/direct"
     weather_url: str = "https://api.openweathermap.org/data/2.5/weather"
     request_timeout: int = 10
@@ -18,3 +20,6 @@ class Settings(BaseSettings):
 
 
 settings = Settings()
+
+if not settings.api_key:
+    raise RuntimeError("API_KEY is not set in environment variables or .env file")
