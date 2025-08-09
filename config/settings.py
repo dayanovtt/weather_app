9786bc24020d5d06facb462cdfd78d99b5a588d0
Timur Dayanov <135445176+dayanovtt@users.noreply.github.com>
2025-08-09 15:41:06 +0300

    fix settings

--- config/settings.py
+++ config/settings.py
@@ -1,12 +1,12 @@
+import os
 from pathlib import Path
 from typing import Optional
-
 from pydantic_settings import BaseSettings, SettingsConfigDict
-from pydantic import Field
+from pydantic import Field, ValidationError
 
 
 class Settings(BaseSettings):
-    api_key: Optional[str] = Field(None, alias="API_KEY")
+    api_key: str = Field(..., alias="API_KEY")
     geo_url: str = "http://api.openweathermap.org/geo/1.0/direct"
     weather_url: str = "https://api.openweathermap.org/data/2.5/weather"
     request_timeout: int = 10
@@ -19,7 +19,16 @@ class Settings(BaseSettings):
     )
 
 
-settings = Settings()
+def get_settings(api_key: Optional[str] = None) -> Settings:
+    if api_key is not None:
+        # передали явно api_key — создаём объект с ним
+        return Settings(api_key=api_key)
+    # иначе загрузка из env и .env файла
+    return Settings()
 
-if not settings.api_key:
-    raise RuntimeError("API_KEY is not set in environment variables or .env file")
+
+try:
+    settings = get_settings(api_key=os.getenv("API_KEY"))
+except ValidationError as e:
+    # Явно ругаемся если ключа нет
+    raise RuntimeError("API_KEY not set in environment or .env") from e
